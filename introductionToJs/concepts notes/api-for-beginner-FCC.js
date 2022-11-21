1b537d4359b9f665b5e7013fdcc35068f138f21b
Riya Gandhi <riya090401@gmail.com>
2022-11-21 16:03:10 +0530

    Add: basics of API

---
+++ introductionToJs/concepts notes/api-for-beginner-FCC.js
@@ -0,0 +1,23 @@
+// mostly when we hear api - it will mean web api but there are more types of api
+// when we type a url/uri in browser -> https: a protocol or rule is followed and a request
+// is made to the server for eg: http GET which means only data will be read no change 
+
+// http is a stateless protocol that is it does not maintain state -> once a request-response 
+// cycle is completed, a new cycle will need to be created
+
+// besides GET, there are more http verbs called REQUEST METHODS
+
+// http headers help us convey data between request ans response -> what does request needs and
+// what was response by server like server status codes => eg: 404 page not found means server
+// could not find the page which was requested
+
+// REST is a like a protocol for API
+// Representational State Transfer
+
+// our request to server is made using http prolly using a library which is stateless : 
+// hence if we need to save login state or do something else, all need to be mentioned in
+// http headers
+
+// GET      POST        PUT PATCH       DELETE      => http verbs
+// Read     Create      Update          Delete      => CRUD Operations
+
